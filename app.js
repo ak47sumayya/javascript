30315d0a3978a735ef5756f9954964cfb09958a8
sumayya farhan <sumayyafarhan22@gmail.com>
2025-09-05 20:50:56 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,172 @@
+Chapter:1       
+          Question :1
+alert("Welcome to my website!");
+         Question :2
+alert("Error!Please enter a valid password");
+        Question :3
+alert("Welcome to JS land...\nHappy Coding!")
+           Question :4
+ {
+   alert("Welcome to JS Land...");
+  alert("Happy Coding!");
+}
+       Question :5
+alert("Hello..I can run JS through my web browser's console");
+        Question :6
+       done in html
+ Question :7
+       done in html
+          _______________
+chapter: 2
+         Question :1
+        var username;
+
+         Question :2
+        var myName;("sumayyaFarhan")
+
+         Question :3
+        var userName = "";
+        var message = "Hello World";
+        alert("message");
+
+         Question :4
+        var candidateName = "sumayyaFarhan";
+        var candidateAge = "18Years";
+        var candidateCourse = "Certified Web And Mobile App Development";
+        alert(candidateName)
+        alert(candidateAge)
+        alert(candidateCourse)
+
+         Question :5
+        var KFC ="Pizza\nPizz\nPiz\nPi\nP"
+        alert(KFC)
+        
+        Question :6
+        var email = "My email address is sumayyafarhan22@gamil.com"
+         alert(email)
+
+        Question :7
+        var book  = " I am trying to learn from 'A smarter\n way to learn JavaScript'";
+        alert(book);
+
+        Question :8
+        DOne in html
+
+        Question :9
+        var Design = "______=+-*$#@______"
+        alert(Design);
+        document.write(Design);
+
+        __________________________
+chapter:3
+         Question  :1
+        var age = "I am 18 Years old"
+        alert(age);
+
+        Question  :2
+        var Visited ="You have visited this 1 times"
+        alert(Visited)
+
+        Question  :3
+        var birthYear =" My birth year is 2006";
+        document.write(birthYear)
+
+        Question :4
+        var visitername ="Ali "
+        var Producttitle ="Clothes"
+        var Quentity ="8Buy"
+        alert (visitername+Quentity+Producttitle )
+
+         ________________________________
+chapter:4
+         Question :1
+         var name,fatherName,age;
+
+          Question :2
+          legalWays:
+        var userName;
+       var $purchase;
+       var _plus;
+       var myprodect5;
+       var fullName;
+       illlegalways:
+      var 123name;   
+      var my-name;   
+      var alert;     
+      var user name; 
+      var @data;
+      
+      Question :4
+      Done in Html
+
+____________________________
+chapter:5
+
+     Question :1
+    var num1 = 5;
+    var num2 = 3;
+     var sum = num1 + num2;
+    document.write("Sum of " + num1 + " and " + num2 + " is " + sum);
+  
+       Question :2
+var num1 = 5;
+var num2 = 3;
+
+document.write("Subtraction: " + (num1 - num2) );
+document.write("Multiplication: " + (num1 * num2) );
+document.write("Division: " + (num1 / num2) );
+document.write("Modulus: " + (num1 % num2));  
+
+         Question :3
+        var sum= Decleared
+        var value="Last Numbers"
+        var num="56";
+        document.write("Initial value+56")
+          var num=("num+ ++x")
+
+        Question :4
+        var ticket=600;
+        var multi=5;
+        document.write(ticket*multi)
+
+        Question :5
+      var num1="4*1=4"
+      document.write(num1) 
+      var num2="4*2=8"
+      document.write(num2) 
+      var num3="4*3=12"
+      document.write(num3) 
+      var num4="4*4=16"
+      document.write(num4) 
+      var num5="4*5=20"
+      document.write(num5) 
+      var num6="4*6=24"
+      document.write(num6) 
+      var num7="4*7=28"
+      document.write(num7) 
+      var num8="4*8=32"
+      document.write(num8) 
+      var num9="4*9=36"
+      document.write(num9) 
+      var num10="4*10=40"
+      document.write(num10)
+
+chapter:6-9
+
+
+ var a = 10;
+    document.write("Result:<br>");
+    document.write("The value of a is: " + a + "<br>");
+    document.write("-------------------------------<br><br>");
+
+    document.write("The value of ++a is: " + (++a) + "<br>");
+    document.write("Now the value of a is: " + a + "<br><br>");
+
+    document.write("The value of a++ is: " + (a++) + "<br>");
+    document.write("Now the value of a is: " + a + "<br><br>");
+
+    document.write("The value of --a is: " + (--a) + "<br>");
+    document.write("Now the value of a is: " + a + "<br><br>");
+
+    document.write("The value of a-- is: " + (a--) + "<br>");
+    document.write("Now the value of a is: " + a + "<br>");
